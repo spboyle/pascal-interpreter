ed72893bb5c410fe232dd7ace66781e676054e71
Sean Boyle <spboyle7@gmail.com>
2020-10-25 20:42:20 -0600

    Plus, Minus, Times, Divide but f***ed up order of operations

--- calc1.py
+++ calc1.py
@@ -3,7 +3,7 @@
 #
 # EOF (end-of-file) token: no more input left for lexical analysis
 INTEGER = 'INTEGER'
-PLUS, MINUS =  'PLUS', 'MINUS'
+PLUS, MINUS, TIMES, DIVIDE =  'PLUS', 'MINUS', 'TIMES', 'DIVIDE'
 EOF = 'EOF'
 
 
@@ -25,8 +25,8 @@ class Interpreter():
         self.pos = 0
         self.current_token = None
 
-    def error(self):
-        raise Exception('Error parsing input')
+    def error(self, msg='Error parsing input'):
+        raise Exception(msg)
 
     def get_next_character(self):
         current_char = ''
@@ -35,25 +35,28 @@ class Interpreter():
             self.pos += 1
         return current_char
 
+    def integer(self, number_token):
+        while self.pos < len(self.text) and self.text[self.pos].isdigit():
+            number_token += self.get_next_character()
+
+        return int(number_token)
+
     def get_next_token(self):
         current_char = self.get_next_character()
         if not current_char:
             return Token(EOF, None)
 
         if current_char.isdigit():
-            number_token = current_char
-            while self.pos < len(self.text) and self.text[self.pos].isdigit():
-                number_token += self.get_next_character()
-
-            token = Token(INTEGER, int(number_token))
-            return token
+            return Token(INTEGER, self.integer(current_char))
 
         if current_char == '+':
-            token = Token(PLUS, current_char)
-            return token
+            return Token(PLUS, current_char)
         elif current_char == '-':
-            token = Token(MINUS, current_char)
-            return token
+            return Token(MINUS, current_char)
+        elif current_char == '*':
+            return Token(TIMES, current_char)
+        elif current_char == '/':
+            return Token(DIVIDE, current_char)
 
         self.error()
 
@@ -66,22 +69,73 @@ class Interpreter():
     def expr(self):
         self.current_token = self.get_next_token()
 
+        operand_stack = []
         left = self.current_token
         self.eat(INTEGER)
 
-        operator = self.current_token
-        if operator.token_type == PLUS:
-            self.eat(PLUS)
-        elif operator.token_type == MINUS:
-            self.eat(MINUS)
+        operand_stack.append(left)
+        while self.current_token.token_type != EOF:
+            if self.current_token.token_type == TIMES:
+                if not operand_stack:
+                    self.error()
+                self.eat(TIMES)
+                left_operand = operand_stack.pop()
+                right_operand = self.current_token
+                self.eat(INTEGER)
+                operand_stack.append(Token(INTEGER, left_operand.value * right_operand.value))
+            elif self.current_token.token_type == DIVIDE:
+                if not operand_stack:
+                    self.error()
+                self.eat(DIVIDE)
+                left_operand = operand_stack.pop()
+                right_operand = self.current_token
+                self.eat(INTEGER)
+                operand_stack.append(Token(INTEGER, left_operand.value / right_operand.value))
 
-        right = self.current_token
-        self.eat(INTEGER)
+            elif self.current_token.token_type == PLUS:
+                if not operand_stack:
+                    self.error()
+                self.eat(PLUS)
+                left_operand = operand_stack.pop()
+                right_operand = self.current_token
+                self.eat(INTEGER)
+                # If next token has higher precedence, save current operation on the stack
+                if self.current_token.token_type in [TIMES, DIVIDE]:
+                    operand_stack.append(left_operand)
+                    operand_stack.append(Token(PLUS, '+'))
+                    operand_stack.append(right_operand)
+                else:
+                    operand_stack.append(Token(INTEGER, left_operand.value + right_operand.value))
 
-        if operator.token_type == PLUS:
-            result = left.value + right.value
-        elif operator.token_type == MINUS:
-            result = left.value - right.value
+            elif self.current_token.token_type == MINUS:
+                if not operand_stack:
+                    self.error()
+                self.eat(MINUS)
+                left_operand = operand_stack.pop()
+                right_operand = self.current_token
+                self.eat(INTEGER)
+                # If next token has higher precedence, save current operation on the stack
+                if self.current_token.token_type in [TIMES, DIVIDE]:
+                    operand_stack.append(left_operand)
+                    operand_stack.append(Token(MINUS, '-'))
+                    operand_stack.append(right_operand)
+                else:
+                    operand_stack.append(Token(INTEGER, left_operand.value - right_operand.value))
+
+        while len(operand_stack) > 2:
+            right = operand_stack.pop()
+            operator = operand_stack.pop()
+            left = operand_stack.pop()
+
+            if operator.token_type == PLUS:
+                operand_stack.append(Token(INTEGER, left.value + right.value))
+            elif operator.token_type == MINUS:
+                operand_stack.append(Token(INTEGER, left.value - right.value))
+
+        if operand_stack:
+            result = operand_stack.pop().value
+        else:
+            self.error('There should be a token on the stack')
 
         return result
 
